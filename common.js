dd928baeca95ba5ddad99b66e5f729959cb0acbc
DavinWang <davin.wang@live.com>
2025-05-05 15:07:18 +0800

    reduce year range for better display

--- common.js
+++ common.js
@@ -1,15 +1,18 @@
+
+var START_YEAR = -4000;
+
 export function formatYear(year) {
     return year < 0 ? `公元前${Math.abs(year)}年` : `公元${year}年`;
 }
 
 export function percentToYear(percent) {
     const currentYear = new Date().getFullYear();
-    return Math.round(-5000 + (currentYear + 5000) * percent / 100);
+    return Math.round(START_YEAR + (currentYear - START_YEAR) * percent / 100);
 }
 
 export function yearToPercent(year) {
     const currentYear = new Date().getFullYear();
-    return Math.round((year + 5000) / (currentYear + 5000) * 100);
+    return Math.round((year - START_YEAR) / (currentYear - START_YEAR) * 100);
 }
 
 // Generate color from dynasty name hash
